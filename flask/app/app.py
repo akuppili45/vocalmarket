2a2525a4224f7f0bb8771dd1e2aab8e4a0ecbb20
akuppili45 <akuppili45@gmail.com>
2023-03-31 22:41:22 +0000

    login/logout with dynamo

--- flask/app/app.py
+++ flask/app/app.py
@@ -6,7 +6,7 @@ from flask import Flask,render_template,flash,redirect, url_for,request, jsonify
 # from flask_sqlalchemy import SQLAlchemy
 import os
 
-from flask_login import LoginManager, login_required, login_user,logout_user
+from flask_login import LoginManager, login_required, login_user,logout_user, current_user
 from flask_login import UserMixin
 from datetime import datetime
 
@@ -14,9 +14,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Email,EqualTo
 
-from flask import Flask,render_template,flash
 
-from  flask_login import LoginManager, login_user, current_user
 
 
 import aws_controller
@@ -25,10 +23,9 @@ import sys
 
 import json
 
+import uuid
+
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
 
 app.app_context().push()
 
@@ -39,74 +36,80 @@ app.config['SECRET_KEY'] = SECRET_KEY
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# class User(UserMixin, db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	username = db.Column(db.String(50), index=True, unique=True)
-# 	email = db.Column(db.String(150), unique = True, index = True)
-# 	password_hash = db.Column(db.String(150))
-# 	joined_at = db.Column(db.DateTime(), default = datetime.utcnow, index = True)
-# 	def set_password(self, password):
-# 		self.password_hash = generate_password_hash(password)
+class User(UserMixin):
+	# id = db.Column(db.Integer, primary_key=True)
+    def __init__(self, username, email, *args, **kwargs):
+        super(User, self).__init__()
+        self.id = str(uuid.uuid4())
+        self.username = username
+        self.email = email
+	# self.password_hash = password_hash
+	# joined_at = db.Column(db.DateTime(), default = datetime.utcnow, index = True)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-# 	def check_password(self,password):
-# 		return check_password_hash(self.password_hash,password)
+    def check_password(self,password):
+	    return check_password_hash(self.password_hash,password)
 
-# class RegistrationForm(FlaskForm):
-#     username = StringField('username', validators =[DataRequired()])
-#     email = StringField('Email', validators=[DataRequired(),Email()])
-#     password1 = PasswordField('Password', validators = [DataRequired()])
-#     password2 = PasswordField('Confirm Password', validators = [DataRequired(),EqualTo('password1')])
-#     submit = SubmitField('Register')
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators =[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password1 = PasswordField('Password', validators = [DataRequired()])
+    password2 = PasswordField('Confirm Password', validators = [DataRequired(),EqualTo('password1')])
+    submit = SubmitField('Register')
 
-# class LoginForm(FlaskForm):
-#     email = StringField('Email',validators=[DataRequired(), Email()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     remember = BooleanField('Remember Me',validators= [DataRequired()])
-#     submit = SubmitField('Login')
-
-
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(user_id)
-
-# @app.route('/home')
-# def home():
-#     return render_template('index.html')
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me',validators= [DataRequired()])
+    submit = SubmitField('Login')
 
 
-# @app.route('/register', methods = ['POST','GET'])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(username =form.username.data, email = form.email.data)
-#         user.set_password(form.password1.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         return redirect(url_for('login'))
-#     return render_template('registration.html', form=form)
+@login_manager.user_loader
+def load_user(user_id):
+    return aws_controller.getUserById(user_id)
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email = form.email.data).first()
-#         if user is not None and user.check_password(form.password.data):
-#             login_user(user)
-#             next = request.args.get("next")
-#             return redirect(next or url_for('home'))
-#         flash('Invalid email address or Password.')    
-#     return render_template('login.html', form=form)
+@app.route('/home')
+def home():
+    # print("current user is " + str(current_user), flush=True)
+    return render_template('index.html', data=current_user)
 
-# @app.route("/logout")
-# # @login_required
-# def logout():
-#     logout_user()
-#     return redirect(url_for('home'))
 
-# @app.route("/forbidden",methods=['GET', 'POST'])
-# @login_required
-# def protected():
-#     return redirect(url_for('forbidden.html'))
+@app.route('/register', methods = ['POST','GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username =form.username.data, email = form.email.data)
+        user.set_password(form.password1.data)
+        aws_controller.addUser(user)
+        return redirect(url_for('login'))
+    return render_template('registration.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user_json = json.loads(json.dumps(aws_controller.getUserByEmail(email = form.email.data)))
+        # print(user_json, flush=True)
+        user = User(user_json['username'], user_json['email'])
+        user.password_hash = user_json['password_hash']
+        user.id = user_json['id']
+        if user is not None and user.check_password(form.password.data):
+            login_user(user)
+            next = request.args.get("next")
+            return redirect(next or url_for('home'))
+        flash('Invalid email address or Password.')    
+    return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/forbidden",methods=['GET', 'POST'])
+@login_required
+def protected():
+    return redirect(url_for('forbidden.html'))
 
 
 @app.route('/get-items')
--- flask/app/aws_controller.py
+++ flask/app/aws_controller.py
@@ -1,9 +1,11 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 
 dynamo_client = boto3.client('dynamodb')
 dynamo_resource = boto3.resource('dynamodb')
 
 SongTable = dynamo_resource.Table('YourTestTable')
+UserTable = dynamo_resource.Table('UserTable')
 
 def get_items():
     return dynamo_client.scan(
@@ -17,3 +19,22 @@ def addSong(artist, songTitle):
             'SongTitle': songTitle
         }
     )    
+def addUser(user):
+    return UserTable.put_item(
+        Item = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'password_hash': user.password_hash
+        }
+    )
+def getUserById(id):
+    return UserTable.query(KeyConditionExpression=Key('id').eq(id))['Items'][0]
+
+def getUserByEmail(email):
+    return UserTable.query(IndexName='email-index', KeyConditionExpression=Key('email').eq(email))['Items'][0]
+
+def get_users():
+    return dynamo_client.scan(
+        TableName='UserTable'
+    )
